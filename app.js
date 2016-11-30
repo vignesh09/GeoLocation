67e197a353eb6e8eea2026205e842ab8706fef9d
Vignesh <vignez.sr@gmail.com>
2016-11-30 23:31:06 +0530

    Add files via upload
    changed ui to fully responsive and social logins added.

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ console.log("welcome");
     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
     $('.modal').modal();
   });
-
+//DOM ELEMENT ID's
+var email=document.getElementById('email');
+var password=document.getElementById('password');
     // Initialize Firebase
    var config = {
      apiKey: "AIzaSyCHGkBU1ntb0w5rO29MeXw-OeP-CE0gM9o",
@@ -14,52 +16,118 @@ console.log("welcome");
      storageBucket: "geolocation-4449e.appspot.com",
      messagingSenderId: "885855350230"
    };
-   firebase.initializeApp(config);
 
-function get_auth(){
    firebase.initializeApp(config);
-  var auth_var=firebase.auth();
-  auth_var.createUserWithEmailAndPassword("haitovignesh.sr@gmail.com", "haitome").then(function(){
+fb=firebase.app();
+
+//Authentication
+function get_auth(){
+  var auth_var=fb.auth();
+  auth_var.createUserWithEmailAndPassword(email.value, password.value).then(function(){
     alert("done successfully");
   }).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // ...
-    console.log("error signing in");
-    console.log(error.message);
-    console.error();
+
+    if(error.message=="The email address is already in use by another account."){
+      fb.auth().signInWithEmailAndPassword(email.value, password.value).then(console.log("you are successfully logged in")).catch(function(error) {
+
+  alert(error.message);
+  });
+    }
+    else{
+  alert(error.message);
+    }
+
   });
 
 }
-//Authentication
-$('#Sign').click(function () {
-get_auth();
+
+function google_auth(){
+  alert("inside google_auth");
+  var provider=new firebase.auth.GoogleAuthProvider();
+  fb.auth().signInWithPopup(provider).then(function(result) {
+  // This gives you a Google Access Token. You can use it to access the Google API.
+  var token = result.credential.accessToken;
+  // The signed-in user info.
+  var user = result.user;
+  alert("done successfully");
+  // ...
+}).catch(function(error) {
+  alert(error);
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+console.error();
+  // ...
+});
+}
+
+
+function facebook_auth(){
+var provider = new firebase.auth.FacebookAuthProvider();
+fb.auth().signInWithPopup(provider).then(function(result) {
+  // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+  var token = result.credential.accessToken;
+  // The signed-in user info.
+  var user = result.user;
+  // ...
+  alert("facebook login successfully");
+}).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+  console.log(error);
+  // ...
 });
 
 
+}
+
+
+function twitter_auth(){
+var provider = new firebase.auth.TwitterAuthProvider();
+fb.auth().signInWithPopup(provider).then(function(result) {
+  // This gives you a the Twitter OAuth 1.0 Access Token and Secret.
+  // You can use these server side with your app's credentials to access the Twitter API.
+  var token = result.credential.accessToken;
+  var secret = result.credential.secret;
+  // The signed-in user info.
+  var user = result.user;
+  // ...
+  alert("twitter login successfully");
+}).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+  console.log(error);
+  // ...
+});
+
+}
 
 
 //database
- var database = firebase.database();
- var starCountRef = firebase.database().ref('Geolocation');
+ var database = fb.database();
+ var starCountRef = fb.database().ref('Geolocation');
 starCountRef.on('value', function(snapshot) {
   console.log(snapshot.val());
   //Get Current position
   function getLocation() {
     console.log("inside getlocation");
       if (navigator.geolocation) {
-          console.log("inside ");
-console.log(navigator.geolocation.getCurrentPosition(function (position){
-  console.log(position);
-}));
-          navigator.geolocation.getCurrentPosition(showPosition);
-console.log("inside and out ");
-
-
-
+              navigator.geolocation.getCurrentPosition(showPosition);
       } else {
-        console.log("not supported")
           alert("Geolocation is not supported by this browser. Search the place you want to goo....");
       }
   }
